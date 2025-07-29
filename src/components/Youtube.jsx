77be4f75312c828295d3418d2c24ef4b894592d9
Cyklan <dev@cyklan.de>
2025-07-29 10:40:40 +0200

    Better youtube test

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,12 +1,13 @@
 import { ConsentGate } from "./ConsentGate";
-
-export function Youtube({ href }) {
+import "./Youtube.css";
+export function Youtube({ id }) {
   return (
     <ConsentGate>
       <iframe
+        className="Youtube"
         width="560"
         height="315"
-        src={href}
+        src={`https://www.youtube.com/embed/${id}`}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
