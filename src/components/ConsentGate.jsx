dcb8f1daa970ea1013e8b45dac626f60a42a2dd7
Cyklan <dev@cyklan.de>
2025-07-29 10:37:15 +0200

    youtube test

--- src/components/ConsentGate.jsx
+++ src/components/ConsentGate.jsx
@@ -1,5 +1,6 @@
 import { useConsent } from "../hooks/useConsent";
 import "./ConsentGate.css";
+import * as Consent from "vanilla-cookieconsent";
 
 export function ConsentGate({ children }) {
   const hasConsented = useConsent();
@@ -7,7 +8,27 @@ export function ConsentGate({ children }) {
   if (!hasConsented) {
     return (
       <div className="ConsentGate">
-        You need to consent to third party stuff first!
+        <h2>Third Party Content</h2>
+        <p>
+          You can't view this third party content because of your{" "}
+          <a href="#" data-cc="show-preferencesModal">
+            Cookie Settings
+          </a>
+          .
+        </p>
+        <button
+          onClick={() => {
+            Consent.acceptCategory("thirdPartyContent");
+          }}
+        >
+          Accept Third Party Content cookies
+        </button>
+        <aside>
+          This website may offer content or functionalities that are provided by
+          third-party providers on their own responsibility. These third-party
+          providers may set their own cookies, e.g. to track user activity or to
+          personalize and optimize their offers.
+        </aside>
       </div>
     );
   }
--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,5 +1,18 @@
 import { ConsentGate } from "./ConsentGate";
 
-export default function Youtube({ href }) {
-  return <ConsentGate>Hallo Mama, ich bin im Fernsehen!</ConsentGate>;
+export function Youtube({ href }) {
+  return (
+    <ConsentGate>
+      <iframe
+        width="560"
+        height="315"
+        src={href}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </ConsentGate>
+  );
 }
